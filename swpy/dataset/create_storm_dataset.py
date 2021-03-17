9bbad3cbd35e0b40a57431607340135cd28298d2
Hirotoshi Uchino <u.hirortoshi@gmail.com>
2021-03-17 21:59:42 +0900

    [Update] create storm dataset

--- swpy/dataset/create_storm_dataset.py
+++ swpy/dataset/create_storm_dataset.py
@@ -1,15 +1,29 @@
-import pyspedas
-from pytplot import get_data, del_data
-
-import pandas as pd
-import numpy as np
 import os
-import sys
-
-# events_dfのData_start, Data_endに含まれるデータを準備
+import argparse
 from glob import glob
 from datetime import datetime, timedelta
 
+import numpy as np
+import pandas as pd
+
+import pyspedas
+from pytplot import get_data, del_data
+
+#######################################
+# argparse
+#######################################
+parser = argparse.ArgumentParser()
+parser.add_argument('--interp', action='store_true')
+parser.add_argument('--targets', dest='targets', default='flow_speed,BZ_GSM,F,proton_density',
+                    help='acceptable physical quantities are "flow_speed", "BZ_GSM", "F", proton_density, "T", "Vx". ')
+
+args = parser.parse_args()
+
+interp = args.interp
+pq_targets = args.targets.split(',')
+pq_targets_str = '-'.join(pq_targets)
+
+# -----------------------------------------------------------
 
 root_data_dir = os.environ['ROOT_DATA_DIR']
 
@@ -19,20 +33,21 @@ root_data_dir = os.environ['ROOT_DATA_DIR']
 kyoto_dir = root_data_dir + 'kyoto/'
 dst_dir  = kyoto_dir + 'DST/'
 
-output_dir = root_data_dir + 'swpy/storm_datasets/Gruet_2018/DST/'
-os.makedirs(output_dir, exist_ok=True)
+dst_output_dir = root_data_dir + 'swpy/storm_datasets/Gruet_2018/DST/'
+os.makedirs(dst_output_dir, exist_ok=True)
 
-# omni_out_dir = '/Volumes/data/swfpy/ml/storm_datasets/basic/omni/'
-omni_out_dir = root_data_dir + 'swpy/storm_datasets/Gruet_2018/omni_5min/'
+omni_out_dir = root_data_dir + 'swpy/storm_datasets/Gruet_2018/omni_5min/%s/' % (pq_targets_str)
 os.makedirs(omni_out_dir, exist_ok=True)
-
 
 # event定義ファイル
 events_file= root_data_dir + 'swpy/storm_datasets/Gruet_2018/magnetic_storm_events.csv'
 
-
+# ------------------------------------------------------------
 
 def make_dst_event_df(start_dt, end_dt, dst_dir):
+    '''
+    イベント期間のDST指数DataFrame作成
+    '''
     def get_DST_file(dt, dst_dir):
         tgt_dir = dst_dir + str(dt.year) + '/'
         glob_pattern = tgt_dir + 'DST_' + str(dt.year)+ str(dt.month).rjust(2, '0') + '*'
@@ -70,16 +85,23 @@ def make_dst_event_df(start_dt, end_dt, dst_dir):
 
     return dst_event_df
 
+# ---
 
 def output2csv_event_df(event_df, start_dt, end_dt, output_dir='./', data_label=''):
+    '''
+    DataFrame の CSV出力用関数
+    '''
     output_path_base = output_dir + 'storm_'
     if data_label != '':
         output_path_base += data_label + '_'
     output_path = output_path_base + start_dt.strftime('%Y%m%d%H') + '_' + end_dt.strftime('%Y%m%d%H') + '.csv'
     event_df.to_csv(output_path, index=False)
 
+# ---
 
-# DST
+# =========================
+# DST データ作成
+# =========================
 event_set_df = pd.read_csv(events_file)
 
 for intdex, event in event_set_df.iterrows():
@@ -92,11 +114,15 @@ for intdex, event in event_set_df.iterrows():
     dst_event_df = make_dst_event_df(start_dt, end_dt, dst_dir)
 
     # ファイルを出力
-    output2csv_event_df(dst_event_df, start_dt, end_dt, output_dir, 'DST')
+    output2csv_event_df(dst_event_df, start_dt, end_dt, dst_output_dir, 'DST')
 
 
-# im_argets = ['Vx', 'BZ_GSM', 'BY_GSM', 'proton_density', 'T']
-targets = ['flow_speed', 'BZ_GSM', 'F', 'proton_density']
+# =========================
+# OMNI データ作成
+# =========================
+
+import pdb
+
 convert_time = np.frompyfunc(lambda x: pyspedas.time_string(x, '%Y%m%d%H%M'), 1, 1)
 
 event_set_df = pd.read_csv(events_file)
@@ -112,11 +138,14 @@ for intdex, event in event_set_df.iterrows():
 
     omni_event_df = pd.DataFrame()
 
-    for target in targets:
+    for target in pq_targets:
         time = get_data(target)[0]
         pq   = get_data(target)[1]
 
         omni_event_df['Time'] = convert_time(time)
         omni_event_df[target] = pq
-    #     print(nf(time), pq_s)
-    output2csv_event_df(omni_event_df, start_dt, end_dt, omni_out_dir, 'OMNI')
+
+    if interp:
+        omni_event_df = omni_event_df.interpolate()
+
+    output2csv_event_df(omni_event_df, start_dt, end_dt, omni_out_dir, 'OMNI_' + pq_targets_str)
