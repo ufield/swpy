e1d2b924a13f0d9b422e0d399fe2e132c555b307
Hirotoshi Uchino <u.hirortoshi@gmail.com>
2021-04-13 22:30:12 +0900

    [Update] adding LSTMPredictorOmniMean

--- swpy/models/predictor.py
+++ swpy/models/predictor.py
@@ -76,3 +76,44 @@ class MlpPredictorOmniMean(nn.Module):
         out = self.linear_3(h4)
 
         return out
+
+
+class LSTMPredictorOmniMean(nn.Module):
+    h_dim_lstm = 20
+    h_dim_fcl  = 5
+
+    def __init__(self, phase, len_dst_p, num_omni_pqs):
+        super().__init__()
+        # super(MlpPredictorOmniMean, self).__init__()
+
+        self.phase = phase
+        self.num_vals = num_omni_pqs + 1
+        self.avgPool1d = nn.AvgPool1d(10, stride=10)
+        self.lstm = nn.LSTM(num_omni_pqs + 1, self.h_dim_lstm * self.num_vals, batch_first=True)
+        # self.linear_1 = FCLayer(self.h_dim_lstm * self.num_vals, self.h_dim_fcl * self.num_vals)
+        self.linear_1 = FCLayer(self.h_dim_lstm * self.num_vals, 1)
+        # self.linear_2 = FCLayer(self.h_dim_fcl * self.num_vals, 1)
+
+    def forward(self, dst_p, omni_data):
+        inputs = torch.zeros(dst_p.shape[0], dst_p.shape[1], self.num_vals)
+
+        inputs[:, :, 0] = dst_p
+        ii = 1
+        for key, val in omni_data.items():
+            val_v = val.view(val.shape[0], 1, val.shape[1])
+            val_a = self.avgPool1d(val_v)
+            val_a = val_a.view(val_a.shape[0], -1)
+            inputs[:, :, ii] = val_a
+            ii += 1
+
+        # import pdb; pdb.set_trace()
+        # LSTMに流す
+        _, (h_n, c_n) = self.lstm(inputs)
+        lstm_out = h_n.view(dst_p.shape[0], -1)
+
+        # 全結合層に流す
+        h_1 = self.linear_1(lstm_out)
+        # out = self.linear_2(h_1)
+        return h_1
+
+        # return out
