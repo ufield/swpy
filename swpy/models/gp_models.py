af69d9c96309ad3675082ebcd39673653941234a
Hirotoshi Uchino <u.hirortoshi@gmail.com>
2021-04-21 22:32:36 +0900

    [Update] dim of mean_x, covar_x

--- swpy/models/gp_models.py
+++ swpy/models/gp_models.py
@@ -1,13 +1,16 @@
 import gpytorch
 from mean_modules import DstNNMean
 
+import pdb
+
 class DstNNExactGP(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, net, likelihood):
         super().__init__(train_x, train_y, likelihood)
         self.mean_module = DstNNMean(net)
+        # TODO: NNKernel の導入
         self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
 
     def forward(self, x):
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
+        mean_x = self.mean_module(x).squeeze() # [batch_size, 1] -> [batch_size]
+        covar_x = self.covar_module(x.view(x.shape[0], -1)) # [batch_size, udt, num_vals] -> [batch_size, udt x num_vals]
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
